5da0884f8b3297f22173fea3127edef75c621153
Timo Stoffregen <timo.stoffregen@outlook.com>
2020-04-15 14:20:39 +1000

    Added source to bash

--- 2d_simulator_generator.sh
+++ 2d_simulator_generator.sh
@@ -4,10 +4,9 @@ do
    echo "========================================================\n"
    echo "This is the $i th image\n"
    echo "========================================================\n"
-   #rm -rf /tmp/event_data
+   source ~/sim_ws/devel/setup.bash
    ct_mean=$(bc <<< "(0.1*$i)+0.1")
    python scripts/generate_esim2d_scenes.py generator_config/slow_motions.json --scene_id=$i --contrast_threshold_mean=$ct_mean --contrast_threshold_sigma=0.1
    python scripts/2d_launch_esim.py --launch_file_path="/tmp/esim.launch"
-   #rm -rf /tmp/event_data
 done
 
